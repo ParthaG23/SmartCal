8f0509d24fb04ee3dcecedb52d3e184a4caaa1a3
Partha Gayen <gayenpartha55@gmail.com>
2026-03-16 19:46:52 +0530

    modify

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
-export const getCalculators = () => API.get("/calculators");
+export const getCalculators = () =>
+  API.get("/calculators");
 
 export const calculate = (type, data) =>
   API.post(`/calculators/${type}`, data);
